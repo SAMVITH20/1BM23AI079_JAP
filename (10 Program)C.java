751b0f96706ccad7cda6f7258c6a3ed0ffa7759e
SAMVITH20 <kanakagiriphani.ai23@bmsce.ac.in>
2025-05-21 15:40:43 +0530

    Create (10 Program)C.java

---
+++ (10 Program)C.java
@@ -0,0 +1,12 @@
+package C;
+import apack.A;
+import bpack.B;
+
+
+
+class C {
+    public void display() {
+        A obj = new A();
+        System.out.println("Public: " + obj.publicVar);
+    }
+}
